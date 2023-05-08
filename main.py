a853a75bb4f9420c7bf49c787e41894bc232f9fa
kubarada <jakub.rd@seznam.cz>
2023-05-08 17:57:25 +0200

    code added

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import os
 
 # Read image
 PATH_TO_IMG = 'cvat_dataset/images/default/SA_20211012-164802_incision_crop_0.jpg'
-IMG_TITLE = 'Incision/Stitch image'
 
 image = cv2.imread(PATH_TO_IMG)
 
@@ -16,8 +15,9 @@ cv2.imwrite('rgb2gray.png', gray)
 
 
 
+
 # Use canny edge detection
-edges = cv2.Canny(gray,60,120,apertureSize=3)
+edges = cv2.Canny(gray,50,100,apertureSize=3)
 kernel = np.ones((1,2),np.uint8)
 edges_morph = cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
 kernel = np.ones((1,5),np.uint8)
@@ -30,19 +30,19 @@ edges_morph = cv2.erode(edges_morph,kernel,iterations = 1)
 
 cv2.imwrite('cannyFilter.png', edges_morph)
 
-
 # Apply HoughLinesP method to
 # to directly obtain line end points
 lines_list = []
 lines = cv2.HoughLinesP(
     edges,  # Input edge image
     20,  # Distance resolution in pixels
-    np.pi / 170,  # Angle resolution in radians
+    np.pi / 180,  # Angle resolution in radians
     threshold=80,  # Min number of votes for valid line
     minLineLength=90,  # Min allowed length of line
-    maxLineGap=18 # Max allowed gap between line for joining them
+    maxLineGap=20 # Max allowed gap between line for joining them
 )
 print(lines)
+
 # Iterate over points
 for points in lines:
     # Extracted points nested in the list
@@ -53,5 +53,4 @@ for points in lines:
     # Maintain a simples lookup list for points
     lines_list.append([(x1, y1), (x2, y2)])
 
-# Save the result image
 cv2.imwrite('detectedLines.png', image)
