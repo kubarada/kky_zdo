58a14294ea6dc1d2f52ef2f5eb3d5534119ce012
kubarada <jakub.rd@seznam.cz>
2023-05-13 17:10:24 +0200

    code added

---
+++ tresholding_Hough.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+PATH_TO_IMG = 'cvat_dataset/images/default/SA_20211104-131923_incision_crop_0.jpg'
+
+img = cv.imread(PATH_TO_IMG, cv.IMREAD_GRAYSCALE)
+h,w = img.shape
+assert img is not None, "file could not be read, check with os.path.exists()"
+img = cv.medianBlur(img,5)
+ret,th1 = cv.threshold(img,110,255,cv.THRESH_BINARY)
+th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+            cv.THRESH_BINARY,11,2)
+th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv.THRESH_BINARY_INV,11,2)
+
+# kernel = np.ones((4,4),np.uint8)
+# kernel = np.ones((1,3),np.uint8)
+# th3 = cv.morphologyEx(th3, cv.MORPH_OPEN, kernel)
+kernel = np.ones((1,3),np.uint8)
+th3 = cv.erode(th3,kernel,iterations = 3)
+
+
+cv.imwrite('tresholded.png', th3)
+
+lines_list = []
+lines = cv.HoughLinesP(
+    th3,  # Input edge image
+    10,  # Distance resolution in pixels
+    np.pi / 180,  # Angle resolution in radians
+    threshold=150,  # Min number of votes for valid line
+    minLineLength=w/1.8,  # Min allowed length of line
+    maxLineGap=10 # Max allowed gap between line for joining them
+)
+print(lines)
+
+# Iterate over points
+for points in lines:
+    # Extracted points nested in the list
+    x1, y1, x2, y2 = points[0]
+    # Draw the lines joing the points
+    # On the original image
+    cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # Maintain a simples lookup list for points
+    lines_list.append([(x1, y1), (x2, y2)])
+
+cv.imwrite('detectedLines.png', img)
